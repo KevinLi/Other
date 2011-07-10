19a8afa9d31f7858c7b86ffdbb9dde636bda038b
Kevin Li <nerd_rage@live.com>
2011-07-10 17:39:54 -0400

    first xchat script!

---
+++ XChat-WDK Python Scripts/invitespam.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+__module_name__ = "invitespam.py"
+__module_version__ = "1.0.0.6"
+__module_description__ = "Joins a user to a specified number of randomly \
+						 named channels, invites a different user to them, \
+						 and parts the first user from all of them."
+__module_author__ = "banhammer"
+
+import xchat
+import random
+
+channels = []
+random.seed()
+
+def main(word, word_eol, userdata):
+	if int(word[2]) <= 15:
+		create_channels(int(word[2]))
+		for i in channels:
+			xchat.command("JOIN {0}".format(i))			
+			xchat.command("INVITE {0}".format(word[1]))
+			xchat.command("PART {0}".format(i))
+	
+def create_channels(number):
+	for i in range(number):
+		channels.append("#" + str(int(random.random() * pow(10, 11))))
+
+#xchat.hook_print('Channel Message', msgparse)
+xchat.hook_command('invitespam', main, help="/invitespam user number")
+print("Loaded {0}, version {1}".format(__module_name__, __module_version__))
